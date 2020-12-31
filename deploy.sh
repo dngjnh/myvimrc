4fda934940851aa7d0c1c6e0aa45182fc507f7f8
dngjnh <dngjnh@gmail.com>
2020-12-31 10:18:07 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /tmp \
-&& git clone https://github.com/junegunn/vim-plug.git \
-&& mkdir -p ~/.vim/autoload \
-&& cp vim-plug/plug.vim ~/.vim/autoload \
-&& cp myvimrc/vimrc ~/.vimrc \
-# && vim -c 'PlugInstall' \
-&& rm -rf vim-plug myvimrc
+git clone https://github.com/junegunn/vim-plug.git && \
+mkdir -p ~/.vim/autoload && \
+cp vim-plug/plug.vim ~/.vim/autoload && \
+cp myvimrc/vimrc ~/.vimrc && \
+# vim -c 'PlugInstall' && \
+rm -rf vim-plug myvimrc
